988f24e6ff303fffdecfcde07b7e714892b36c4a
KartoonYoko <crashed9001@gmail.com>
2023-12-02 15:13:42 +0900

    ...

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/KartoonYoko/go-url-shortener/internal/app"
 )
 
 func main() {
 	app.Serve()
-
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Got panic", r)
-			panic("Something gone wrong")
-		}
-	}()
 }
