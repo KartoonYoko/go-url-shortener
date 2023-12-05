db4caab509e86c79d633b75fe58dd2ef7879bc67
KartoonYoko <crashed9001@gmail.com>
2023-12-06 05:13:39 +0900

    ...

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 type Config struct {
@@ -16,8 +17,17 @@ func New() *Config {
 	b := flag.String("b", "http://localhost:8080", "Flag responsible for base addres of shorted url")
 	flag.Parse()
 
-	return &Config{
+	c := &Config{
 		BootstrapNetAddress: *a,
 		BaseURLAddress:      *b,
 	}
+
+	if envServerAddr := os.Getenv("SERVER_ADDRESS"); envServerAddr != "" {
+        c.BootstrapNetAddress = envServerAddr
+    }
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+        c.BaseURLAddress = envBaseURL
+    }
+
+	return c
 }
