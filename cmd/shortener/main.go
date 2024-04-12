6327b24397aa9c4cb0c4f362a365a5b4c7deb807
KartoonYoko <crashed9001@gmail.com>
2024-04-12 21:20:03 +0900

    added build variables

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/KartoonYoko/go-url-shortener/internal/app"
 )
 
+var buildVersion string = "N/A"
+var buildDate string = "N/A"
+var buildCommit string = "N/A"
+
 func main() {
+	fmt.Printf("Build version: %s", buildVersion)
+	fmt.Printf("Build date: %s", buildDate)
+	fmt.Printf("Build commit: %s", buildCommit)
 	app.Run()
 }
